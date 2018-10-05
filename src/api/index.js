4a49ee5b681f4bda689b739cbff5b5ff44d1bb5a
cdiezmoran <cdiezmoran@gmail.com>
2018-10-05 01:45:36 -0500

    Update user route

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,14 @@ import { version } from '../../package.json';
 import { Router } from 'express';
 import auth from './auth';
 import files from './files';
+import user from './user';
 
 export default () => {
 	let api = Router();
 
 	api.use('/', auth);
 	api.use('/', files);
+	api.use('/', user);
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
