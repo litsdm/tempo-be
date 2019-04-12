e8e22e6e84855dcc800e20bcacd51f2318d8a577
cdiezmoran <cdiezmoran@gmail.com>
2019-04-12 15:52:01 -0500

    Add preflight option from cors

--- src/index.js
+++ src/index.js
@@ -37,6 +37,8 @@ app.use(cors({
 	exposedHeaders: config.corsHeaders,
 }));
 
+app.options('*', cors())
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
