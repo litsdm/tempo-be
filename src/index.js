3772574bd530ba9828e7fce34eef3bbb69ca1f5e
cdiezmoran <cdiezmoran@gmail.com>
2019-04-12 15:55:22 -0500

    Add whitelist

--- src/index.js
+++ src/index.js
@@ -33,7 +33,15 @@ app.server = http.createServer(app);
 app.use(morgan('dev'));
 
 // 3rd party middleware
+const whitelist = ['https://www.feathershare.com']
 app.use(cors({
+	origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
 	exposedHeaders: config.corsHeaders,
 }));
 
