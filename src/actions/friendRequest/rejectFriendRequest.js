69be17db5c9ed2ead2e508bf94b82e223fdf2101
cdiezmoran <cdiezmoran@gmail.com>
2019-04-25 13:46:42 -0500

    Refactor friend requests endpoint

---
+++ src/actions/friendRequest/rejectFriendRequest.js
@@ -0,0 +1,35 @@
+import FriendRequest from '../../models/friendRequest';
+
+const deleteFriendRequest = (requestId) => new Promise((resolve, reject) => {
+  FriendRequest.findOneAndDelete({ _id: requestId }, (error) => {
+    if (error) reject(error);
+    resolve();
+  });
+});
+
+const validateOptions = (options) => {
+  try {
+    if (!options) throw new Error('options object is required.');
+    if (!options.params) throw new Error('options.params is required.');
+    if (!options.params.requestId) throw new Error('options.params.requestId is required.');
+    if (!options.response) throw new Error('options.response is required.');
+  } catch(exception) {
+    throw new Error(`[rejectFriendRequest.validateOptions] ${exception.message}`);
+  }
+}
+
+const rejectFriendRequest = async ({ params }, response) => {
+  try {
+    const { requestId } = params;
+    validateOptions({ params, response });
+
+    await deleteFriendRequest(requestId);
+
+    response.status(200).send();
+  } catch(exception) {
+    response.status(500).send({ message: exception.message });
+    throw new Error(`[rejectFriendRequest] ${exception.message}`)
+  }
+}
+
+export default rejectFriendRequest;
--- src/api/friendRequest.js
+++ src/api/friendRequest.js
@@ -1,69 +1,15 @@
 import { Router } from 'express';
 
-import FriendRequest from '../models/friendRequest';
-import User from '../models/user';
+import createFriendRequest from '../actions/friendRequest/createFriendRequest';
+import getFriendRequests from '../actions/friendRequest/getFriendRequests';
+import acceptFriendRequest from '../actions/friendRequest/acceptFriendRequest';
+import rejectFriendRequest from '../actions/friendRequest/rejectFriendRequest';
 
 const router = Router();
 
-router.post('/friendRequest', ({ body: { tag, from, queryProperty } }, res) => {
-  const property = queryProperty || 'tag';
-  User.findOne({ [property]: tag }, (err, user) => {
-    if (!user || err) return res.status(401).send({ message: 'Hm, didn\'t work. Double check that the capitalization, spelling, any spaces, and numbers are correct.' });
-    if (isInArray(from, user.friends)) return res.status(200).send({ message: `${user.username} is already your friend!` });
-
-    FriendRequest.findOne({ from, to: user._id }, (error, friendReq) => {
-      if (error) return res.status(401).send({ message: 'Hm, didn\'t work. Double check that the capitalization, spelling, any spaces, and numbers are correct.' });
-      if (friendReq) return res.status(200).send({ message: 'Your request has already been sent!' });
-
-      const friendRequest = new FriendRequest({ from, to: user._id });
-      friendRequest.save((error) => {
-        if (error) return res.status(400).send({ message: 'Hm, didn\'t work. We were unable to send your request, please try again later.' });
-
-        res.status(200).send({ friendRequest });
-      })
-    });
-  });
-});
-
-router.get('/friendRequest/:user_id', ({ params: { user_id } }, res) => {
-  FriendRequest.find({ to: user_id })
-    .populate('from', '_id username placeholderColor')
-    .exec((err, friendRequests) => {
-      if (err) return res.status(400).send({ message: err.message });
-
-      res.status(200).send({ friendRequests });
-    });
-});
-
-router.delete('/friendRequest/:request_id/accept', ({ params: { request_id } }, res) => {
-  FriendRequest.findById(request_id).exec((err, friendRequest) => {
-    if (!friendRequest) return res.status(404).send({ message: 'Friend Request not found.' })
-    addFriendToUser(friendRequest.to, friendRequest.from, (err) => {
-      if (err) return res.status(400).send({ message: err.message });
-      addFriendToUser(friendRequest.from, friendRequest.to, (error) => {
-        if (error) return res.status(400).send({ message: err.message });
-
-        friendRequest.remove();
-        res.status(200).send();
-      });
-    });
-  });
-});
-
-router.delete('/friendRequest/:request_id/reject', ({ params: { request_id } }, res) => {
-  FriendRequest.findOneAndDelete({ _id: request_id }, (err) => {
-    if (err) return res.status(400).send({ message: err.message });
-    res.status(200).send();
-  });
-});
-
-const addFriendToUser = (userId, friendId, cb) => {
-  User.findOneAndUpdate({ _id: userId }, { $push: { friends: friendId } })
-    .exec((err) => {
-      cb(err);
-    });
-}
-
-const isInArray = (checkId, array) => array.some(friend => friend.equals(checkId));
+router.post('/friendRequest', createFriendRequest);
+router.get('/friendRequest/:userId', getFriendRequests);
+router.delete('/friendRequest/:requestId/accept', acceptFriendRequest);
+router.delete('/friendRequest/:requestId/reject', rejectFriendRequest);
 
 export default router;
