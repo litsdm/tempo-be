ae9a17d9e601c01c444827368d53be16ac271318
cdiezmoran <cdiezmoran@gmail.com>
2019-07-15 16:07:35 -0500

    Switch to oauth2 for email

---
+++ src/actions/email/sendLink.js
@@ -0,0 +1,61 @@
+import path from 'path';
+import nodemailer from 'nodemailer';
+import Email from 'email-templates';
+import { google } from 'googleapis';
+
+const { OAuth2 } = google.auth;
+const { GMAIL_CLIENT_ID, GMAIL_SECRET, REFRESH_TOKEN, EMAIL_USER } = process.env;
+
+const validateOptions = options => {
+  try {
+    if (!options) throw new Error('options object is required.');
+    if (!options.body) throw new Error('options.body is required.');
+    if (!options.body.to) throw new Error('options.body.to is required.');
+    if (!options.body.endpoint) throw new Error('options.body.endpoint is required.');
+    if (!options.response) throw new Error('options.response is required.');
+  } catch (exception) {
+    throw new Error(`[sendLink.validateOptions] ${exception.message}`);
+  }
+}
+
+const sendLink = async ({ body }, response) => {
+  try {
+    const { to, endpoint, from } = body;
+    validateOptions({ body, response });
+
+    const client = new OAuth2(GMAIL_CLIENT_ID, GMAIL_SECRET, 'https://developers.google.com/oauthplayground');
+    client.setCredentials({ refresh_token: REFRESH_TOKEN });
+    const accessToken = await client.getAccessToken();
+
+    const transport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: EMAIL_USER,
+        clientId: GMAIL_CLIENT_ID,
+        clientSecret: GMAIL_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken
+      }
+    });
+
+    const mailOptions = {
+      template: path.join(__dirname, '../../emails/link'),
+      message: { to: to.join(',') },
+      locals: {
+        url: `https://www.feathershare.com/${endpoint}`,
+        from
+      }
+    };
+
+    const email = new Email({ message: { from: EMAIL_USER }, transport, send: true });
+    await email.send(mailOptions);
+
+    response.status(200).send();
+  } catch (exception) {
+    response.status(500).send();
+    throw new Error(`[sendLink] ${exception.message}`);
+  }
+}
+
+export default sendLink;
--- src/api/email.js
+++ src/api/email.js
@@ -1,43 +1,9 @@
-import path from 'path';
-import nodemailer from 'nodemailer';
-import Email from 'email-templates';
 import { Router } from 'express';
 
-const { EMAIL_USER, EMAIL_PASS } = process.env;
-
-const transport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: EMAIL_USER,
-    pass: EMAIL_PASS
-  }
-});
+import sendLink from '../actions/email/sendLink';
 
 const router = Router();
 
-router.post('/email', ({ body: { to, endpoint, from } }, res) => {
-  const email = new Email({
-    message: {
-      from: EMAIL_USER
-    },
-    transport
-  });
-
-  email.send({
-    template: path.join(__dirname, '../emails/link'),
-    message: {
-      to: to.join(' ')
-    },
-    locals: {
-      url: `https://www.feathershare.com/${endpoint}`,
-      from
-    }
-  })
-    .then(() => res.sendStatus(200))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).send({ err })
-    });
-});
+router.post('/email', sendLink);
 
 export default router;
